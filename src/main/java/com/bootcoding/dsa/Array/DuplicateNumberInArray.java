72cd69fa7d7e637d022a69403b8ec744e5abe2f3
AbhishekB06 <abhishekab852000@gmail.com>
2023-01-16 23:49:51 +0530

    Print duplicate number from array

---
+++ src/main/java/com/bootcoding/dsa/Array/DuplicateNumberInArray.java
@@ -0,0 +1,28 @@
+package com.bootcoding.dsa.Array;
+
+import java.util.Scanner;
+
+public class DuplicateNumberInArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of Array :");
+        int size = sc.nextInt();
+        int numbers[] = new int[size];
+        for(int i = 0; i < numbers.length; i++){
+            System.out.println("Enter element "+(i+1));
+            int number = sc.nextInt();
+            numbers[i] =number;
+        }
+        System.out.println("Duplicate Number :");
+        duplicateNumber(numbers);
+    }
+    public static void duplicateNumber(int[] numbers){
+        for (int i = 0; i < numbers.length; i++){
+            for(int j = i+1; j < numbers.length; j++){
+                if(numbers[i] == numbers[j]){
+                    System.out.println(numbers[i]);
+                }
+            }
+        }
+    }
+}
